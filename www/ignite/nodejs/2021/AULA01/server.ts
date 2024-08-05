7df5697fd4b6ca2053aa92ad7048020d283a8ee4
Renato Lemos Mendes <renato.lemos@ciclocairu.com.br>
2024-08-05 18:00:16 -0400

    feat: 7 commit

---
+++ www/ignite/nodejs/2021/AULA01/server.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma =new PrismaClient();
+
+async function main() {
+    const result = await prisma.courses.create({
+        data: {
+            duration: 200,
+            name: "Curso de NodeJs",
+            description: "Curso excelente de NodeJS"
+        }
+    })
+console.log(result)    
+}
